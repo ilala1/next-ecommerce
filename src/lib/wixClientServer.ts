c43c9017d5b3575137b51f74b78a796ec3b9cb9e
imran <imranlala@hotmail.co.uk>
2024-06-13 19:35:45 +0100

    added profile page

--- src/lib/wixClientServer.ts
+++ src/lib/wixClientServer.ts
@@ -2,7 +2,7 @@ import { OAuthStrategy, createClient } from "@wix/sdk";
 import { collections, products } from "@wix/stores";
 // import { orders } from "@wix/ecom";
 import { cookies } from "next/headers";
-// import { members } from '@wix/members';
+import { members } from '@wix/members';
 
 export const wixClientServer = async () => {
   let refreshToken;
@@ -17,7 +17,7 @@ export const wixClientServer = async () => {
       products,
       collections,
     //   orders,
-    //   members,
+      members,
     },
     auth: OAuthStrategy({
       clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID!,
