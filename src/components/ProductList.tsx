80e34bb674484775982c432894dd9810f41a778f
imran <imranlala@hotmail.co.uk>
2024-06-13 11:50:58 +0100

    added pagination

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -3,6 +3,7 @@ import { products } from "@wix/stores";
 import Image from "next/image";
 import Link from "next/link";
 import DOMPurify from "isomorphic-dompurify";
+import Pagination from "./Pagination";
 // import Pagination from "./Pagination";
 
 const PRODUCT_PER_PAGE = 8;
@@ -35,7 +36,7 @@ const ProductList = async ({
 				: 0
 		);
 	// .find();
-		let res;
+	let res;
 
 	if (searchParams?.sort) {
 		const [sortType, sortBy] = searchParams.sort.split(" ");
@@ -45,77 +46,80 @@ const ProductList = async ({
 		}
 		if (sortType === "desc") {
 			res = await productQuery.descending(sortBy).find();
-		} 
+		}
 		if (sortType === "Sort") {
 			res = await productQuery.find();
 		}
-	} else {
+	}
+
+	if (res === undefined) {
 		res = await productQuery.find();
 	}
 
 	return (
 		<div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
-			{res && res.items.map((product: products.Product) => (
-				<Link
-					href={"/" + product.slug}
-					className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-					key={product._id}
-				>
-					<div className="relative w-full h-80">
-						<Image
-							src={
-								product.media?.mainMedia?.image?.url ||
-								"/product.png"
-							}
-							alt=""
-							fill
-							sizes="25vw"
-							className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-						/>
-						{product.media?.items && (
+			{res &&
+				res.items.map((product: products.Product) => (
+					<Link
+						href={"/" + product.slug}
+						className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+						key={product._id}
+					>
+						<div className="relative w-full h-80">
 							<Image
 								src={
-									product.media?.items[1]?.image?.url ||
+									product.media?.mainMedia?.image?.url ||
 									"/product.png"
 								}
 								alt=""
 								fill
 								sizes="25vw"
-								className="absolute object-cover rounded-md"
+								className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
 							/>
+							{product.media?.items && (
+								<Image
+									src={
+										product.media?.items[1]?.image?.url ||
+										"/product.png"
+									}
+									alt=""
+									fill
+									sizes="25vw"
+									className="absolute object-cover rounded-md"
+								/>
+							)}
+						</div>
+						<div className="flex justify-between">
+							<span className="font-medium">{product.name}</span>
+							<span className="font-semibold">
+								${product.price?.price}
+							</span>
+						</div>
+						{product.additionalInfoSections && (
+							<div
+								className="text-sm text-gray-500"
+								dangerouslySetInnerHTML={{
+									__html: DOMPurify.sanitize(
+										product.additionalInfoSections.find(
+											(section: any) =>
+												section.title === "shortDesc"
+										)?.description || ""
+									),
+								}}
+							></div>
 						)}
-					</div>
-					<div className="flex justify-between">
-						<span className="font-medium">{product.name}</span>
-						<span className="font-semibold">
-							${product.price?.price}
-						</span>
-					</div>
-					{product.additionalInfoSections && (
-						<div
-							className="text-sm text-gray-500"
-							dangerouslySetInnerHTML={{
-								__html: DOMPurify.sanitize(
-									product.additionalInfoSections.find(
-										(section: any) =>
-											section.title === "shortDesc"
-									)?.description || ""
-								),
-							}}
-						></div>
-					)}
-					<button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
-						Add to Cart
-					</button>
-				</Link>
-			))}
-			{/* {searchParams?.cat || searchParams?.name ? (
-        <Pagination
-          currentPage={res.currentPage || 0}
-          hasPrev={res.hasPrev()}
-          hasNext={res.hasNext()}
-        />
-      ) : null} */}
+						<button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
+							Add to Cart
+						</button>
+					</Link>
+				))}
+			{searchParams?.cat || searchParams?.name ? (
+				<Pagination
+					currentPage={res.currentPage || 0}
+					hasPrev={res.hasPrev()}
+					hasNext={res.hasNext()}
+				/>
+			) : null}
 		</div>
 	);
 };
